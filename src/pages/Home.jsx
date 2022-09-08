c76131c7db9937b00f9315df42c74e0001bff2b1
aliadabi <aliadabi3780@gmail.com>
2022-09-08 10:54:34 -0700

    Added files and also Fetched Data from Spoonacular API

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Veggie from "../components/Veggie/Veggie";
+import Popular from "../components/Popular/Popular";
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <Veggie/>
+            <Popular/>
+        </div>
+    );
+}
+
+export default Home;
