66f715c35d27887c9604ec8ecffd74b27a310396
aliadabi <aliadabi3780@gmail.com>
2022-09-12 14:58:26 -0700

    Added New Files -> Category.jsx and Cuisine.jsx and Added BrowserRouter to main App.js file - also Added routing for diffrent cuisines

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category/Category";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+        <Router>
+
+        <Category/>
         <Pages/>
+
+        </Router>
 
     </div>
   );
